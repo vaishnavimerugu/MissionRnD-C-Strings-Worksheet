134bf11f9e34bf43eada29fd78f01856a09781df
vaishnavimerugu <vaish.reddy05@gmail.com>
2015-11-29 03:05:43 +0530

    Submit Commit

--- src/commonWords.cpp
+++ src/commonWords.cpp
@@ -15,7 +15,67 @@ NOTES: If there are no common words return NULL.
 #include <malloc.h>
 
 #define SIZE 31
-
-char ** commonWords(char *str1, char *str2) {
+char * mystrtok(char *s, char delim)
+{
+	int i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == delim)
+		{
+			return &s[++i];
+		}
+		else
+			i++;
+	}
 	return NULL;
+}
+char ** commonWords(char *str1, char *str2)
+{
+	if (str1 == NULL&&str2 == NULL)
+		return NULL;
+	char *pos1; pos1 = str1;
+	int j = 0;
+	char **result = (char **)malloc(10 * sizeof(char *));
+	while (pos1 != NULL)
+	{
+		char *pos2; pos2 = str2;
+		while (pos2 != NULL)
+		{
+			char *temp1, *temp2;
+			temp1 = pos1; temp2 = pos2;
+			int count = 0, flag = 1;
+			while (*temp1 != ' ' && *temp2 != ' ')
+			{
+				if (*temp1 != *temp2)
+				{
+					flag = 0;
+					break;
+				}
+				temp1++;
+				temp2++;
+				count++;
+			}
+			if (count > 0 && flag)
+			{
+				char *temp;
+				temp = pos1; int k = 0;
+				result[j] = (char *)malloc(31 * sizeof(char));
+				while (count > 0)
+				{
+					result[j][k] = *temp;
+					k++;
+					temp++;
+					count--;
+				}
+				result[j++][k] = '\0';
+
+			}
+			pos2 = mystrtok(pos2, ' ');
+		}
+		pos1 = mystrtok(pos1, ' ');
+	}
+	if (j == 0)
+		return NULL;
+	else
+		return result;
 }
